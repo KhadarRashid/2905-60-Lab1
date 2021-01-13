028a763977a34cc3a4f3848722ac0b60eb756f54
KhadarRashid <60489828+KhadarRashid@users.noreply.github.com>
2021-01-13 16:10:55 -0600

    Update helloPython.py

--- helloPython.py
+++ helloPython.py
@@ -1,8 +1,10 @@
+# Gathering inputs
 name = input('Hello there, what is your name? ')
 bday = input(f'When were you born {name}? ')
-
+# Letting them know of the number of letters in their name
 print(f"btw your name has {len(name)} letters")
 
+#checking to see if their birthday is in January
 if bday.lower() == 'january':
     print(f'What are the chances, its your birthday month! Happy birthday, belated or early! Whichever is the case {name}')
 else: 
