a875d0a71f13d490756008b52f430c9953513dbf
KhadarRashid <60489828+KhadarRashid@users.noreply.github.com>
2021-01-13 16:09:26 -0600

    Update camelCase.py

--- camelCase.py
+++ camelCase.py
@@ -1,6 +1,11 @@
-txt = input(" please enter something to be turned into camelCase").title()
+# Getting the input and capitalizing first letter of every word
+txt = input(" please enter something to be turned into camelCase").title() 
+ # Splitting it into a list
 txtList = txt.split( )
-txtList2 = ("".join(txtList))
+#Joining the list to get rid of whitespace
+txtList2 = ("".join(txtList)) 
+# Isolating the first letter to make it lowercase and extracting the rest of the 
+# letters and adding it to the camelCase variable
 camelCase = txt[0:1].lower() + txtList2[1:]
 
 print(camelCase)
