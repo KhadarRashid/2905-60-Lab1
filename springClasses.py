b437eb775f67a6ffc7d20bd9df027920f874bcb5
KhadarRashid <60489828+KhadarRashid@users.noreply.github.com>
2021-01-13 16:12:45 -0600

    Update springClasses.py

--- springClasses.py
+++ springClasses.py
@@ -1,11 +1,16 @@
 
+# Creating an empty list to append to later
 classList = []
+
 print("What classes will you be taking this semester?: ")
+# Gathering the classes they will be taking
 classes = input("Please enter all the classes you are taking this semester (press enter to quit): ")
 while classes:
+    # Appending the classes to the list
     classList.append(classes)
     classes = input("Please enter all the classes you are taking this semester (press enter to quit): ")
 
+# Showing the classes they will be taking even numbered
 print("You will be taking these classes this semester")
 for index, c in enumerate(classList):
     print(f"{index+1} {c}")
